531f6623b1d6423b499184bf5e73b5887249840e
Eugene Tan <jmingtan@gmail.com>
2009-12-23 11:03:22 +0800

    Simple twisted client

--- sandbox/client.py
+++ sandbox/client.py
@@ -1,5 +1,15 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 6123))
-s.send('HELLO WORLD')
-s.close()
+from twisted.internet.protocol import Protocol, ClientCreator
+from twisted.internet import reactor
+
+class Greeter(Protocol):
+	def sendMessage(self, msg):
+		self.transport.write("msg: %s" % msg)
+
+def gotProtocol(p):
+	p.sendMessage("Hello")
+	reactor.callLater(1, p.sendMessage, "This is sent in a sec")
+	reactor.callLater(2, p.transport.loseConnection)
+
+c = ClientCreator(reactor, Greeter)
+c.connectTCP("127.0.0.1", 6123).addCallback(gotProtocol)
+reactor.run()
